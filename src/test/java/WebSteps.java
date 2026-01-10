1a5709a9a27f66db2f352fc39386cc1119547b75
Anastasiya Ursu <a-ursu20@yandex.ru>
2026-01-10 14:16:18 +0300

    Removed unused imports and commented code

--- src/test/java/WebSteps.java
+++ src/test/java/WebSteps.java
@@ -1,17 +1,11 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Issue;
 import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.qameta.allure.Allure.step;
+
 
 public class WebSteps {
 
@@ -52,34 +46,5 @@ public class WebSteps {
 
 
     }
-
-
-    /*@Test
-    public void testLambdaStep() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Открываем главную страницу", () -> {
-            open("https://github.com");
-        });
-
-        step("Ищем репозиторий " + REPOSITORY, () -> {
-            $(".header-search-button").click();
-            $("#query-builder-test").setValue(REPOSITORY);
-            $("#query-builder-test").submit();
-        });
-
-        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
-            $(By.linkText(REPOSITORY)).click();
-        });
-
-        step("Кликаем на таб Issues", () -> {
-            $("#issues-tab").click();
-        });
-
-        step("Проверяем наличие текста 'forTests'.", () -> {
-            $(withText("forTests")).should(exist);
-        });
-
-    }*/
 }
 
